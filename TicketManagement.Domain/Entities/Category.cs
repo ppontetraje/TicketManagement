be7f20f4b1bb8a876663992f2cf208ddf54a92c0
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2021-03-28 18:47:10 -0500

    Agregar archivos de proyecto.

---
+++ TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketManagement.Domain.Common;
+using TicketManagement.Domain.Entities;
+
+namespace TicketManagement.Domain
+{
+    class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
