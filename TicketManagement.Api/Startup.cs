614e7c6f389c05e3c25ea23ab17668af2a3a7925
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2021-04-05 17:56:30 -0500

    Se utilizó el paquete nugget Swashbuckle y Swagger. Se configuracó Swagger en la clase Startup para que se muestre la documentación del API.

--- TicketManagement.Api/Startup.cs
+++ TicketManagement.Api/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using TicketManagement.Api.Utility;
 using TicketManagement.Application;
 using TicketManagement.Infraestructure;
 using TicketManagement.Persistence;
@@ -21,6 +23,8 @@ namespace TicketManagement.Api
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            AddSwagger(services);
+
             services.AddApplicationServices();
             services.AddInfrastructureServices(Configuration);
             services.AddPersistenceServices(Configuration);
@@ -30,6 +34,20 @@ namespace TicketManagement.Api
             services.AddCors(options =>
             {
                 options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+        }
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Ticket Management API",
+
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
             });
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +60,12 @@ namespace TicketManagement.Api
 
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket Management API");
+            });
 
             app.UseCors("Open");
 
